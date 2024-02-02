ccb84245204a885ee5141474a3a058c673982fcb
Saurav <choudharysaurav724@gmail.com>
2024-02-03 02:03:04 +0530

    read data from DB

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,13 @@
+const Product = require('../models/product');
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ message: "Get all Products List" });
+    const All_Products = await Product.find({});
+    res.status(200).json({ All_Products });
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ message: "ProductsList Testing" });
+    const All_Products = await Product.find({});
+    res.status(200).json({ All_Products });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
